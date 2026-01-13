88a967c285998acb0b2e5cf4cda689946ac9f6b6
Akram Imtiaz <akram.imtiaz10@gmail.com>
2026-01-14 00:27:12 +0800

    add logout button on home screen

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,10 +1,14 @@
+import { use } from "react";
+import { AuthContext } from "@/ contexts/auth-context";
 import { Text } from "@react-navigation/elements";
-import { View } from "react-native";
+import { Button, View } from "react-native";
 
 export default function Home() {
+  const { logOut } = use(AuthContext);
   return (
     <View>
       <Text>Home Screen!</Text>
+      <Button title="Log Out" onPress={logOut} />
     </View>
   );
 }
